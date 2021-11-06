e8489b9304d5dd419e9238689e0bc1d063d7cac8
Vladislav Startsev <vlad288583@gmail.com>
2021-11-06 22:45:49 +0500

    create websocket, added socketsend, check socketisavailable

--- osucket/Program.cs
+++ osucket/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Fleck;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using TagLib.NonContainer;
 
 namespace osucket
 {
     class Program
     {
         static void Main(string[] args)
+        { 
+            var server = new WebSocketServer("ws://0.0.0.0:13371");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => new Thread(() =>
+                {
+                    GetMemoryInfo(socket);
+                }).Start();
+                socket.OnClose = () => Console.WriteLine("Close!");
+                socket.OnMessage = message => socket.Send(message);
+            });
+
+            while (true)
+            {
+
+            }
+        }
+
+        static async void GetMemoryInfo(IWebSocketConnection socket)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                socket.Send("test");
+                await Task.Delay(5000);
+                if (socket.IsAvailable == false)
+                {
+                    return;
+                }
+            }
         }
     }
 }
